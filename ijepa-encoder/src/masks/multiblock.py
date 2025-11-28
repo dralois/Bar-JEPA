d04a28fa144728405150d477ebd8ff9540d22604
Alexander Epple <alexander.epple@uni-ulm.de>
2025-11-28 16:04:27 +0100

    Merged some open pull requests - Fixes bfloat support - Adds error handling - Fixes mask size

--- ijepa-encoder/src/masks/multiblock.py
+++ ijepa-encoder/src/masks/multiblock.py
@@ -64,9 +64,9 @@ class MaskCollator(object):
         # -- Compute block height and width (given scale and aspect-ratio)
         h = int(round(math.sqrt(max_keep * aspect_ratio)))
         w = int(round(math.sqrt(max_keep / aspect_ratio)))
-        while h >= self.height:
+        while h > self.height:
             h -= 1
-        while w >= self.width:
+        while w > self.width:
             w -= 1
 
         return (h, w)
@@ -86,8 +86,8 @@ class MaskCollator(object):
         valid_mask = False
         while not valid_mask:
             # -- Sample block top-left corner
-            top = torch.randint(0, self.height - h, (1,))
-            left = torch.randint(0, self.width - w, (1,))
+            top = torch.randint(0, self.height - h + 1, (1,))
+            left = torch.randint(0, self.width - w + 1, (1,))
             mask = torch.zeros((self.height, self.width), dtype=torch.int32)
             mask[top:top+h, left:left+w] = 1
             # -- Constrain mask to a set of acceptable regions
