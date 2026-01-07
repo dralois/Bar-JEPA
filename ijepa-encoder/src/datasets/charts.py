fix: Origin ground truth calculation

ijepa-encoder/src/datasets/charts.py
@@ -152,18 +152,18 @@ class Charts(torchvision.datasets.DatasetFolder):
 
         # Coordinate system origin is normalized
         size = torch.tensor([float(v) for v in ann['chart_metadata']['size']['bbox'][2:]])
-        gt_org = torch.tensor([float(v) for v in ann['chart_metadata']['origin']['bbox'][:2]]) / size
+        gt_org = (torch.tensor([float(v) for v in ann['chart_metadata']['origin']['bbox'][:2]]) / size).flip(-1)
 
         ticks = []
         # Ticks are normalized x,y coordinates of the tick location
         for tick in ann['data']['value_axis']['ticks']:
-            ticks.append(torch.tensor([float(v) for v in tick['bbox'][:2]]) / size)
+            ticks.append((torch.tensor([float(v) for v in tick['bbox'][:2]]) / size).flip(-1))
 
         bars = []
         # Bars are normalized x,y coordinates of a bar's top left corner
         for feature in ann['data']['features']:
             for bar in feature['data']:
-                bars.append(torch.tensor([float(v) for v in bar['bbox'][:2]]) / size)
+                bars.append((torch.tensor([float(v) for v in bar['bbox'][:2]]) / size).flip(-1))
 
         # Map size depends on image size
         mapsize = (torch.tensor(img.shape[1:3]) // self.patch_size) * 4

ijepa-encoder/src/utils/heatmap.py
@@ -26,8 +26,8 @@ def cls_pts_to_map(
     for cls_id, cls_list in enumerate(cls_pts_lists):
         for point in cls_list:
             # Compute map coordinates and regression values
-            pos = torch.floor(point.flip(-1) * mapsize).type(torch.int32)
-            reg = (point.flip(-1) * mapsize) - pos - 0.5
+            pos = torch.floor(point * mapsize).type(torch.int32)
+            reg = (point * mapsize) - pos - 0.5
 
             # Store in maps
             cls_map[pos[0], pos[1]] = cls_id + 1
@@ -127,8 +127,8 @@ def p_maps_to_cls_lists(
         conf = torch.sigmoid(p_cls[2, point[0], point[1]])
         ticks.append((pos, conf))
 
-    # Select highest confidence candidate from origin heatmap
-    org_idx = torch.argmax(torch.sigmoid(p_cls[3]))
+    # Select highest confidence candidate from background-masked origin heatmap
+    org_idx = torch.argmax(torch.sigmoid(p_cls[3]) * pts_mask)
     org_point = torch.stack(torch.unravel_index(org_idx, p_cls[3].shape))
     org_pos = (org_point * 2 + 1) / (size * 2)
     # Offset pixel midpoint by regression value